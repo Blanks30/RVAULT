3851395958a5d967b636f321b06f748099e159a5
Blanks30 <Blanks30@users.noreply.github.com>
2026-09-21 22:41:38 +0530

    Connect Instagram authentication

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,5 @@
+require("dotenv").config();
+
 const express = require("express");
 const contentRoutes = require("./routes/content");
 
@@ -7,6 +9,20 @@ app.use(express.json());
 
 app.get("/", (req, res) => {
     res.send("RVAULT is running!");
+});
+
+app.get("/instagram/status", (req, res) => {
+    if (!process.env.INSTAGRAM_ACCESS_TOKEN) {
+        return res.status(500).json({
+            connected: false,
+            error: "Instagram access token is missing"
+        });
+    }
+
+    res.json({
+        connected: true,
+        message: "Instagram access token is loaded!"
+    });
 });
 
 app.use("/", contentRoutes);
